0dd1dcb05776225ab3560e682cce2f9a44bd7dc0
a736399919 <55206982+a736399919@users.noreply.github.com>
2022-03-17 09:24:16 +0800

    Update Tmall_M1.sh

--- Tmall_M1/Tmall_M1.sh
+++ Tmall_M1/Tmall_M1.sh
@@ -7,7 +7,7 @@
 #=================================================
 #克隆源码
 #git clone -b openwrt-18.06-k5.4 --single-branch https://github.com/immortalwrt/immortalwrt openwrt
-git clone -b openwrt-18.06 --single-branch https://github.com/immortalwrt/immortalwrt openwrt
+git clone -b master --single-branch https://github.com/immortalwrt/immortalwrt openwrt
 cd openwrt
 ./scripts/feeds clean
 ./scripts/feeds update -a
