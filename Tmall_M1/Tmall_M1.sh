569e10e7d059c399d39a813ace822c5831643dde
a736399919 <55206982+a736399919@users.noreply.github.com>
2022-03-24 20:04:02 +0800

    Update Tmall_M1.sh

--- Tmall_M1/Tmall_M1.sh
+++ Tmall_M1/Tmall_M1.sh
@@ -6,9 +6,8 @@
 #   Blog: https://p3terx.com
 #=================================================
 #克隆源码
-#git clone -b openwrt-18.06-k5.4 --single-branch https://github.com/immortalwrt/immortalwrt openwrt
+git clone -b openwrt-18.06-k5.4 --single-branch https://github.com/immortalwrt/immortalwrt openwrt
 #git clone -b openwrt-22.03 --single-branch https://github.com/openwrt/openwrt
-git clone -b openwrt-18.06 --single-branch https://github.com/immortalwrt/immortalwrt openwrt
 cd openwrt
 
 #删除包含"routing"的行
